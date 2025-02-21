e2344a4e84f1d4d3ed20c71fc8ee84ca066caf27
aditya <asadityasingh2572000@gmail.com>
2025-02-21 23:57:22 +0530

    add re-set component

---
+++ admin/src/components/ReserPasswordPage.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom' // or next/router for Next.js
+import axios from 'axios'
+
+export default function ResetPasswordPage() {
+  const { token } = useParams()
+  const [newPassword, setNewPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await axios.post('http://localhost:8006/api/reset-password', {
+        token,
+        newPassword,
+      })
+      alert(res.data.message)
+    } catch (error) {
+      alert(error.response.data.message)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Reset Password</h2>
+      <input
+        type="password"
+        placeholder="Enter your new password"
+        value={newPassword}
+        onChange={(e) => setNewPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Reset Password</button>
+    </form>
+  )
+}
