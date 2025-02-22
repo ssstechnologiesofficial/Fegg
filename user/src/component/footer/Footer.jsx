e524ff80e9a2a58097a80e675fbcfe3e09fe96c3
aditya <asadityasingh2572000@gmail.com>
2025-02-22 12:38:38 +0530

    chnage footer propertyes

--- user/src/component/footer/Footer.jsx
+++ user/src/component/footer/Footer.jsx
@@ -65,9 +65,9 @@ const Footer = () => {
           </div>
 
           {/* Links */}
-          <div className="flex flex-col items-center md:items-start footer-section">
+          <div className="flex flex-col md:items-start items-center text-center md:text-start footer-section">
             <h3 className="text-lg font-semibold text-red-400 mb-4">Links</h3>
-            <ul className="space-y-2">
+            <ul className="md:space-y-2 space-x-0">
               <li>
                 <Link to="/about-us" className="hover:underline">
                   About Us
