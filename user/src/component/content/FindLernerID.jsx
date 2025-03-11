03bc9d67dceb8a435a12d322255c1d273b0a4e96
schhaya <schhaya144@gmail.com>
2025-03-11 11:18:43 +0530

    learner id

--- user/src/component/content/FindLernerID.jsx
+++ user/src/component/content/FindLernerID.jsx
@@ -77,8 +77,8 @@ const FindLernerID = () => {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center w-full p-6">
-      <h2 className="text-2xl sm:text-5xl font-semibold text-white text-center my-5 border-[#fd645b] border-x-4 bg-[#00043c] pt-1 py-1 w-full">
+    <div className="flex flex-col justify-center items-center w-full p-6 sm:px-12 px-4">
+      <h2 className="text-2xl sm:text-5xl font-semibold text-white text-center my-5 border-[#fd645b] border-x-4 bg-[#fd645b] pt-1 py-1 w-full">
         Know Your Student Learner ID
       </h2>
 
